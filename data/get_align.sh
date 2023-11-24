627927ffe181250e0a45c915c1215e360571ab3d
manhph2211 <maxph22112000@gmail.com>
2023-11-24 18:36:04 +0800

    .

--- data/get_align.sh
+++ data/get_align.sh
@@ -1,10 +1,10 @@
 #dataset="$1"
-cd /home/max/coding/portaspeech/data/custom_data/
+cd /home/max/coding/ViTTS/data/custom_data/
 for dataset in */
 do
   if [ -d "$dataset" ];
   then
-    save_folder="/home/max/coding/portaspeech/data/preprocessed_data/TextGrid/${dataset}"
+    save_folder="/home/max/coding/ViTTS/data/preprocessed_data/TextGrid/${dataset}"
     if [ -d "$save_folder" ]
     then
       echo "Already align this dataset!"
@@ -17,10 +17,10 @@ do
     if [[ "$dataset" == *"LJ"* ]]
     then
       mfa model download acoustic english_mfa
-      mfa align /home/max/coding/portaspeech/data/custom_data/${dataset} english_mfa /home/max/coding/portaspeech/data/preprocessed_data/TextGrid/${dataset}
+      mfa align /home/max/coding/ViTTS/data/custom_data/${dataset} english_mfa /home/max/coding/ViTTS/data/preprocessed_data/TextGrid/${dataset}
     else
       mfa model download acoustic vietnamese_mfa
-      mfa train --config_path /home/max/coding/portaspeech/data/mfa_config.yml /home/max/coding/portaspeech/data/custom_data --clean /home/max/coding/portaspeech/data/lexicon/lexicon.dict /home/max/coding/portaspeech/data/preprocessed_data/TextGrid/${dataset}
+      mfa train --config_path /home/max/coding/ViTTS/data/mfa_config.yml /home/max/coding/ViTTS/data/custom_data --clean /home/max/coding/ViTTS/data/lexicon/lexicon.dict /home/max/coding/ViTTS/data/preprocessed_data/TextGrid/${dataset}
     fi
   fi
 done
